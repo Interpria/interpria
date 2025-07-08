338ee0de5e86acd22c4ba92ff57d624ec17cfb88
Tommie Tong <interpria@outlook.com>
2025-07-08 18:57:22 -0400

    Change the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() { 
     return( 
         <footer className="text-center py-1" style={{ backgroundColor: "var(--light-background)" }}> 
-            <p className='m-0'>&copy; 2025 Interpria. All rights revserced.</p>
+            <p className='m-0'>&copy; 2025 Interpria. All rights reserved.</p>
         </footer> 
     ) 
 }
